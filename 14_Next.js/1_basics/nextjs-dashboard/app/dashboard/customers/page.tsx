c9da0eca166dd39d94fe400ebbf242b4f9464ea0
Alnyle <corgoo7z49h@gmail.com>
2025-05-11 16:02:25 +0300

    add file

--- 14_Next.js/1_basics/nextjs-dashboard/app/dashboard/customers/page.tsx
+++
@@ -1,8 +0,0 @@
-
-const Page = () => {
-  return (
-    <div>Customer Page</div>
-  )
-}
-
-export default Page
--- Python/10.modules.py
+++
@@ -1,9 +0,0 @@
-import math
-import decimal
-
-print("the value of pi = ", math.pi)
-
-
-print(dir(decimal))
-
-
